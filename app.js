b8a7774818709a38fd26fc32391749cdb8a48525
Akele Benjamin <akelebenjamin.cleon@gmail.com>
2023-11-14 02:04:06 -0500

    Excercise 3

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
+const LINK= 'http://localhost/info2180-lab4/superheroes.php';
+let result=document.getElementById('#result');
+let input= document.querySelector("input");
+let userInput= input.value.trim();
+let request=`http://localhost/info2180-lab4/superheroes.php?name=${userInput}`;
+
+
 
 document.getElementById('searchButton').addEventListener('click',function() {
-    let link= 'http://localhost/info2180-lab4/superheroes.php';
-    fetch(link)
+    
+    fetch(request)
         .then(response=> {
             if(response.ok){return response.text()}
             else{return Promise.reject('Something went wrong!')}
         })
         .then(data=> {
-            alert(data)
+            result.innerHTML= data;
+            console.log(data);
         })
         .catch(error=> {
             console.log(error);
-        
+
         });
 });
 console.log("End of Javscript File");
